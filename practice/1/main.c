4db26a3f7d9bda0d3606ba5164ba093e3fce8960
Artur <71281020+Artur0412-ku@users.noreply.github.com>
2023-07-03 12:54:24 +0300

    Update main.c

--- practice/1/main.c
+++ practice/1/main.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int t1, t2, t3;  // змінні t1, t2 і t3, які зберігають введені значення часу для кожного гостя
-    double total_time;  //  загальний час, який потрібен гостям, щоб з’їсти один пиріг
+    int t1, t2, t3; // змінні t1, t2 і t3, які зберігають введені значення часу для кожного гостя
+    printf("Print three values: "); 
+    scanf("%d %d %d", &t1, &t2, &t3);
 
-    // Зчитуємо вхідні дані
-    printf("Print the time of first guest: ");
-    scanf("%d", &t1);
-    printf("Print the time of second guest: ");
-    scanf("%d", &t2);
-    printf("Print the time of third guest: ");
-    scanf("%d", &t3);
-
-    // Обчислюю загальний час
-    total_time = t1 + t2 + t3;
-
-    //Обчислюю час в годинах, поділивши загальний час на 3
-    double time_in_hours = total_time / 3.0;
-
-    printf("Guests can eat the cake in %.2f hours \n", time_in_hours);
+    float total_time = (t1 + t2 + t3) / 3.0; //  загальний час, який потрібен гостям, щоб з’їсти один пиріг
+    printf("Guests can eat the cake in %.2f hours \n", total_time);
 
     return 0;
-
-
 }
